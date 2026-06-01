a9a2a6ce9414bce50b80ec6362685a872327231c
ThuNg <minhthunguyenir@gmail.com>
2026-06-01 19:00:10 +0700

    feat: hoàn thành API lấy danh sách user cho cả admin và super-admin

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -16,7 +16,27 @@ class Users(models.Model):
     remember_token = models.CharField(max_length=100, db_collation='SQL_Latin1_General_CP1_CI_AS', blank=True, null=True)
     created_at = models.DateTimeField(blank=True, null=True)
     updated_at = models.DateTimeField(blank=True, null=True)
+    USERNAME_FIELD = 'email'          # Chỉ định trường 'email' làm tên đăng nhập chính (thay cho 'username' mặc định của Django)
+    REQUIRED_FIELDS = []              # Các trường bắt buộc phải nhập thêm khi tạo tài khoản bằng lệnh cmd (mình để trống là ok)
 
+    @property
+    def is_authenticated(self):
+        """Trả về True để báo cho Django biết: Đây là tài khoản ĐÃ ĐĂNG NHẬP THÀNH CÔNG"""
+        return True
+
+    @property
+    def is_anonymous(self):
+        """Trả về False để khẳng định: Đây là người dùng thật, KHÔNG PHẢI KHÁCH ẨN DANH"""
+        return False
+
+    @property
+    def is_active(self):
+        """
+        Bắt bài hệ thống: Tận dụng luôn cái trường 'status' của nhóm Thư.
+        Nếu trạng thái là 'Active' thì trả về True (cho phép hoạt động), ngược lại thì khóa.
+        """
+        return self.status == 'Active'
+    
     class Meta:
         managed = False
         db_table = 'users'
